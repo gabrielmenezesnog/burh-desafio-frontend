58c70091c8b10d9d5ba85206b78aebc8e5fe37d8
gabrielmenezesn <gabriel.menezes@grupoirrah.com>
2024-06-09 00:23:15 -0300

    feat: adicionando ícone de deleção

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import "./style.scss";
 import App from "./App.vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCirclePlus, faCalendar } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCirclePlus,
+  faCalendar,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import router from "./router";
 
-library.add(faCirclePlus, faCalendar);
+library.add(faCirclePlus, faCalendar, faTrash);
 
 createApp(App)
   .use(router)
