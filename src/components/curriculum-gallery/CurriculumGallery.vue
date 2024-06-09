4863ccc30bd9bacac291c7242c17834f8d23394f
gabrielmenezesn <gabriel.menezes@grupoirrah.com>
2024-06-09 00:22:51 -0300

    feat: atualizando galeria de currículos após deletar um item

--- src/components/curriculum-gallery/CurriculumGallery.vue
+++ src/components/curriculum-gallery/CurriculumGallery.vue
@@ -1,10 +1,40 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CurriculumPreview from "../curriculum-preview/CurriculumPreview.vue";
+import { onMounted, ref } from "vue";
+import { api } from "../../api";
+import { iFormValues } from "../../interfaces/iFormValues";
+
+const formsArray = ref<iFormValues[]>();
+
+onMounted(async () => {
+  await fetchForms();
+});
+
+const fetchForms = async () => {
+  try {
+    await api.get(`/curriculum`).then((res) => (formsArray.value = res.data));
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
 
 <template>
   <section aria-label="currículos gerados">
-    <h1 class="empty--gallery">
+    <h1 v-if="formsArray && formsArray.length === 0" class="empty--gallery">
       Você ainda não possui nenhum currículo. Crie um agora!
     </h1>
+
+    <ul class="curriculum--gallery">
+      <li
+        v-if="formsArray && formsArray.length > 0"
+        v-for="(form, index) in formsArray"
+        :key="index"
+        class="gallery--item"
+      >
+        <CurriculumPreview :formValues="form" @formDeleted="fetchForms" />
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -15,6 +45,18 @@
   font-weight: 400;
   color: #595959;
   line-height: calc(1.5rem * 1.5);
+}
+
+.curriculum--gallery {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  gap: 20px;
+  flex-wrap: wrap;
+  margin-top: 60px;
+}
+
+.gallery--item {
+  z-index: 0;
 }
 
 @media (max-width: 768px) {
